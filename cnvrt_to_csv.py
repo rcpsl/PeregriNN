748b93ba44b375bdf683693d15a1ba5047b6f232
Haitham Khedr <HaithamKhedr@gmail.com>
2021-04-19 11:54:43 -0700

    Remove extra files

--- cnvrt_to_csv.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-read  = pd.read_csv(r'Acas_results.txt')
-read.columns = ['network','prop','result','time']
-read.to_csv (r'results/Acas_results.csv', index=None)
-
-read  = pd.read_csv(r'mnist_results.txt')
-read.columns = ['network','eps','result','time']
-read.to_csv (r'results/mnist_results.csv', index=None)
--- run_acas_hard.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python -u  property1.py hard >> acas_hard.txt 
-
-python -u  property2.py hard >> acas_hard.txt 
-
-python -u  property3.py hard >> acas_hard.txt 
-
-python -u  property9.py hard >> acas_hard.txt 
